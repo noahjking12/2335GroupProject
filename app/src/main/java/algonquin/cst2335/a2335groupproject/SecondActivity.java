0602577d6a7fde494920ca006d2a5f4c94e3d674
Yalin <karen.s2886@gmail.com>
2023-03-19 01:25:32 -0400

    added SharedPreferences to save the user input of width and height when user click the save button, display the user's saved input when the application is launched next time

--- app/src/main/java/algonquin/cst2335/a2335groupproject/SecondActivity.java
+++ app/src/main/java/algonquin/cst2335/a2335groupproject/SecondActivity.java
@@ -6,6 +6,8 @@ import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,6 +16,7 @@ import android.widget.ImageView;
 import com.bumptech.glide.Glide;
 
 
+import java.io.File;
 import java.util.ArrayList;
 
 import algonquin.cst2335.a2335groupproject.data.CatListViewModel;
@@ -29,6 +32,7 @@ public class SecondActivity extends AppCompatActivity {
     private RecyclerView.Adapter myAdapter;
 
     private CatListViewModel catModel;
+    private SharedPreferences myPreferences;
 
 
     @Override
@@ -38,7 +42,29 @@ public class SecondActivity extends AppCompatActivity {
         binding2 = ActivitySecondBinding.inflate(getLayoutInflater());
         setContentView(binding2.getRoot());
 
+
+
         binding2.recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        //File sandbox = new File(getFilesDir(), "favourite.png");
+
+        myPreferences = getPreferences(Context.MODE_PRIVATE);
+        String widthFound = myPreferences.getString("Width","not found");
+        String heightFound = myPreferences.getString("Height","not found");
+        binding2.widthEdit.setText(widthFound);
+        binding2.heightEdit.setText(heightFound);
+
+        binding2.saveButton.setOnClickListener( click -> {
+            String widthToSaved = binding2.widthEdit.getText().toString();
+            String heightToSaved = binding2.heightEdit.getText().toString();
+            SharedPreferences.Editor editor = myPreferences.edit();
+            editor.putString("Width", widthToSaved);
+            editor.putString("Height", heightToSaved);
+
+            editor.apply();
+
+        });
+
 
         catModel = new ViewModelProvider(this).get(CatListViewModel.class);
         imageUrlList = catModel.catViewModel.getValue();
@@ -59,8 +85,8 @@ public class SecondActivity extends AppCompatActivity {
                 CatList clObj = new CatList(catUrl);
                 imageUrlList.add(clObj);
                 myAdapter.notifyItemInserted(imageUrlList.size() - 1);
-                binding2.widthEdit.setText("");
-                binding2.heightEdit.setText("");
+                //binding2.widthEdit.setText("");
+                //binding2.heightEdit.setText("");
             }
         });
         binding2.recyclerView.setAdapter(myAdapter = new RecyclerView.Adapter<myCatHolder>() {
